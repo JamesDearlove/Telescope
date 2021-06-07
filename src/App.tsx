a1337f537a43e3ceaa6d280cad30bf6ebaa98a49
James Dearlove <jamesdearlove@outlook.com>
2021-06-07 22:52:02 +1000

    List of todoist tasks

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,61 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
+import axios from "axios";
 
 const TODOIST_KEY = ""
 
-const Todoist = () => {
+const axiosInstance = axios.create({
+  baseURL: "https://api.todoist.com/rest/v1/",
+  headers: { Authorization: `Bearer ${TODOIST_KEY}`}
+  
+})
 
+interface TodoistItem {
+  id: number
+  project_id: number
+  section_id: number
+  content: string
+  description: string
+  completed: boolean
+  label_ids: number[]
+  parent_id: number
+  order: number
+  priority: 1 | 2 | 3 | 4 | undefined
+  due: TodoistDue
+  url: string
+  comment_count: number
+  assignee: number
+  assigner: number
+}
 
+interface TodoistDue {
+  string: string
+  date: string
+  recurring: boolean
+  datetime?: string
+  timezone?: string
+}
 
-  return <></>
+const TodoItems = () => {
+
+  const [items, setItems] = useState<TodoistItem[]>([]);
+
+  useEffect(() => {
+    const syncTodoist = async () => {
+      const result = await axiosInstance.get("tasks")
+
+      console.log(result.data)
+      setItems(result.data)
+    }
+
+    syncTodoist()
+  }, [])
+
+  return (
+    <div>
+      {items.map((item) => (<p>{item.content}</p>))}
+    </div>
+  );
 }
 
 function App() {
@@ -35,6 +83,8 @@ function App() {
       {options.map((item) => (
         <p>{item}</p>
       ))}
+
+      <TodoItems />
     </div>
   );
 }
