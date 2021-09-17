3dd551f011d32c2a46ea690747e0517188f1112b
James Dearlove <jamesdearlove@outlook.com>
2021-09-17 18:20:10 +1000

    Remove Tailwind. Added Chakra

--- src/index.tsx
+++ src/index.tsx
@@ -4,13 +4,16 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { QueryClient, QueryClientProvider } from "react-query";
+import { ChakraProvider } from "@chakra-ui/react";
 
 const queryClient = new QueryClient();
 
 ReactDOM.render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
-      <App />
+      <ChakraProvider>
+        <App />
+      </ChakraProvider>
     </QueryClientProvider>
   </React.StrictMode>,
   document.getElementById("root")
